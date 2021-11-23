29032cc47c162f7f25a65e5724815703e0b48e2c
ouwargui <guikemon2@gmail.com>
2021-11-22 22:56:52 -0300

    finish project

---
+++ pipe.js
@@ -0,0 +1,81 @@
+const HOLE_HEIGHT = 220;
+const PIPE_WIDTH = 120;
+const PIPE_INTERVAL = 1500;
+const PIPE_SPEED = .75;
+let pipes = [];
+let timeSinceLastPipe;
+let passedPipeCount;
+
+export function setupPipes() {
+    document.documentElement.style.setProperty("--pipe-width", PIPE_WIDTH);
+    document.documentElement.style.setProperty("--hole-height", HOLE_HEIGHT);
+    pipes.forEach(pipe => pipe.remove());
+    timeSinceLastPipe = PIPE_INTERVAL;
+    passedPipeCount = 0;
+}
+
+export function updatePipes(delta) {
+    timeSinceLastPipe += delta;
+
+    if (timeSinceLastPipe > PIPE_INTERVAL) {
+        timeSinceLastPipe -= PIPE_INTERVAL;
+        createPipe();
+    }
+
+    pipes.forEach(pipe => {
+        if (pipe.left + PIPE_WIDTH < 0) {
+            passedPipeCount++;
+            return pipe.remove();
+        }
+        pipe.left = pipe.left - delta * PIPE_SPEED;
+    });
+}
+
+export function getPassedPipesCount() {
+    return passedPipeCount;
+}
+
+export function getPipeRects() {
+    return pipes.flatMap(pipe => pipe.rects());
+}
+
+function createPipe() {
+    const pipeElement = document.createElement("div");
+    const topElement = createPipeSegment("top");
+    const bottomElement = createPipeSegment("bottom");
+    pipeElement.append(topElement);
+    pipeElement.append(bottomElement);
+    pipeElement.classList.add("pipe");
+    pipeElement.style.setProperty("--hole-top", randomNumberBetween(HOLE_HEIGHT * 1.5, window.innerHeight - HOLE_HEIGHT * 0.5));
+    const pipe = {
+        get left() {
+            return parseFloat(getComputedStyle(pipeElement).getPropertyValue("--pipe-left"));
+        },
+        set left(value) {
+            pipeElement.style.setProperty("--pipe-left", value);
+        },
+        remove() {
+            pipes = pipes.filter(p => p !== pipe);
+            pipeElement.remove();
+        },
+        rects() {
+            return [
+                topElement.getBoundingClientRect(),
+                bottomElement.getBoundingClientRect()
+            ];
+        }
+    }
+    pipe.left = window.innerWidth;
+    document.body.append(pipeElement);
+    pipes.push(pipe);
+}
+
+function createPipeSegment(position) {
+    const segment = document.createElement(position);
+    segment.classList.add("segment", position);
+    return segment;
+}
+
+function randomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
